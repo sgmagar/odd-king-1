061677eee81c89e2fe322353fc4a277f5f5b8b59
Saurav <sp.gharti@gmail.com>
2023-03-08 16:30:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "odd-king-1",
+  url: "https://odd-king-1.botics.co",
+  api: "https://odd-king-1.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
